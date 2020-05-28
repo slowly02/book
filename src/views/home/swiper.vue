973c887df39a76fdba7eb62ebb2ea32bcca7ed83
slowly02 <slowlyhg960327@163.com>
2020-05-28 17:19:51 +0800

    tijiao

--- src/views/home/swiper.vue
+++ src/views/home/swiper.vue
@@ -3,11 +3,11 @@
       <!-- {{list}} -->
   <div class="swiper-container">
         <div class="swiper-wrapper">
-            <div class="swiper-slide" style="background-image:url(https://www.swiper.com.cn/demo/images/nature-1.jpg)"></div>
-            <div class="swiper-slide" style="background-image:url(https://www.swiper.com.cn/demo/images/nature-2.jpg)"></div>
-            <div class="swiper-slide" style="background-image:url(https://www.swiper.com.cn/demo/images/nature-3.jpg)"></div>
-            <div class="swiper-slide" style="background-image:url(https://www.swiper.com.cn/demo/images/nature-4.jpg)"></div>
-            <div class="swiper-slide" style="background-image:url(https://www.swiper.com.cn/demo/images/nature-5.jpg)"></div>
+            <div class="swiper-slide" style="background-image:url(https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1590664729026&di=441ea62cd55a8c057de28f9162bc9e3b&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201804%2F22%2F20180422230950_wBXFU.jpeg)"></div>
+            <div class="swiper-slide" style="background-image:url(https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1590664729024&di=792471e7e19daad68a4c7ccd114b1402&imgtype=0&src=http%3A%2F%2Fcdn.duitang.com%2Fuploads%2Fitem%2F201410%2F17%2F20141017162807_4JuXA.thumb.700_0.jpeg)"></div>
+            <div class="swiper-slide" style="background-image:url(https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1590664729021&di=a54e1896fd13e74eda5b228701e4805e&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2Faa644751b8e6dd77dbc66d16280a923ab90dc41b2c642-9pCEpt_fw658)"></div>
+            <div class="swiper-slide" style="background-image:url(https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1590664729020&di=bf0f4a0791aa56c5b2be31b594bcb323&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201704%2F03%2F20170403010002_TECUP.png)"></div>
+            <div class="swiper-slide" style="background-image:url(https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1590664729019&di=7d425d49fd707cb99511781a0ac096b8&imgtype=0&src=http%3A%2F%2Fimg.kuai8.com%2Fattaches%2Falbum%2F20140614%2F201406141636477351.jpg)"></div>
     </div>
     <!-- Add Pagination -->
     <div class="swiper-pagination"></div>
